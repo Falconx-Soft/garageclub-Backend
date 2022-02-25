00d9e4fa759b79f534606bfcc488b20d4d7bf7f2
Tayyab <F2017065009@umt.edu.pk>
2022-02-25 22:04:52 +0500

    margin issue

--- validaciones/api/serializers.py
+++ validaciones/api/serializers.py
@@ -32,39 +32,39 @@ class ValidationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Validation
         exclude = ['deleted_at', 'updated_at']
-    #     depth = 1
+        depth = 1
 
-    # def create(self, validated_data):
-    #     costs = validated_data.pop('costs')
-    #     print(costs)
+    def create(self, validated_data):
+        costs = validated_data.pop('costs')
+        print(costs)
 
-    #     costs = map(lambda cost: {**cost, "amount": cost['cost'].amount * cost['quantity']}, costs)
-    #     costs = list(map(lambda cost: CostQuantity.objects.create(**cost), costs))
+        costs = map(lambda cost: {**cost, "amount": cost['cost'].amount * cost['quantity']}, costs)
+        costs = list(map(lambda cost: CostQuantity.objects.create(**cost), costs))
 
-    #     validation = Validation.objects.create(**validated_data)
-    #     print(costs)
+        validation = Validation.objects.create(**validated_data)
+        print(costs)
 
-    #     validation.costs.add(*costs)
-    #     costs_sum = sum(cost.amount for cost in costs)
-    #     print(costs_sum)
+        validation.costs.add(*costs)
+        costs_sum = sum(cost.amount for cost in costs)
+        print(costs_sum)
 
-    #     if validation.calculation_type == 1:
-    #         net_margin = validation.amount_purchase - validation.amount_sale
-    #         net_margin = net_margin - costs_sum
-    #         print(net_margin)
+        # if validation.calculation_type == 1:
+        #     net_margin = validation.amount_purchase - validation.amount_sale
+        #     net_margin = net_margin - costs_sum
+        #     print(net_margin)
 
-    #     elif validation.calculation_type == 0:
+        # elif validation.calculation_type == 0:
             
-    #         validation.sale_vat = True
+        #     validation.sale_vat = True
             
-    #         gross_margin = validation.amount_sale - validation.amount_purchase
-    #         net_margin = gross_margin / (1 + 0.21) 
+        #     gross_margin = validation.amount_sale - validation.amount_purchase
+        #     net_margin = gross_margin / (1 + 0.21) 
 
-    #         net_margin = net_margin - costs_sum
-    #         print(net_margin)
+        #     net_margin = net_margin - costs_sum
+        #     print(net_margin)
 
-    #     validation.margin = float('{:.2f}'.format(net_margin))
-    #     validation.save()
+        # validation.margin = float('{:.2f}'.format(net_margin))
+        validation.save()
 
 
-    #     return validation
+        return validation
